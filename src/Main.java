af5f06f13857c47c52bfc06d1ba6114a9bb45bb8
shamilgaleyev <galeyevshamil@gmail.com>
2023-03-31 21:47:31 -0400

    Solar

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Solar solar = new Solar();
+        solar.planet= "8";
+        solar.sun = "1";
+    }
+}
---
+++ src/Solar.java
@@ -0,0 +1,5 @@
+public class Solar {
+    String sun;
+    String planet;
+
+}
